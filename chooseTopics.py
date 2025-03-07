9b4027d58090b1590ed0e90f278731487083d719
Paul Klarer <paul.klarer@outlook.com>
2025-03-07 14:18:20 +0100

    add system prompt for topic

--- chooseTopics.py
+++ chooseTopics.py
@@ -6,7 +6,9 @@ import helper.data.importFunctions as read
 zeitungen =read.newspapers()
 modelID = constants.MODEL_ID
 TopicsJSON = constants.TOPICS_JSON_PATH
-structure='{"Thema 1", "Thema 2", "Thema 3"} Füge keine anderen Sonderzeigen hinzu.Die antwort soll eine valide python liste sein.'
+structure='{"Thema 1", \n "Thema 2", \n "Thema 3"} Füge keine anderen Sonderzeigen hinzu.'
+system_prompt = f'Du bist ein Redactionsassitent bei einer Zeitung und musst 3 Artiklevorschäge für die heuteige Zeitung aus einer Themenliste aussuchen. Deine Artikelvorschläge soll folgende Struktur haben: {str(structure)}'
+systemMessage = [{"role": "system","content": system_prompt}]
 
 def chooseTopics():
     homepage = read.homepage()
@@ -15,9 +17,9 @@ def chooseTopics():
     topics_dict = {}
     for z in zeitungen:
         print('Topic for Zeitung')
-        prompt = f"Wähle aus der Liste 3 passende Themen für die Zeitung {z['Name']}an Hand dieser Themen aus.  {z['Name']} operiert unter dem Motto {z['Motto']} und schriebt am meisten über {z['Themen']}. Gib eine Lsite mit 3 passenden Themen zurück über die {z['Name']} heute berichten kann. Die Liste ist structuriert {str(structure)} "
+        prompt = f"Wähle aus der Liste 3 passende Themen für die Zeitung {z['Name']}an Hand dieser Themen aus.  {z['Name']} operiert unter dem Motto {z['Motto']} und schriebt am meisten über {z['Themen']}. Gib eine Lsite mit 3 passenden Themen zurück über die {z['Name']} heute berichten kann. "
         final_msg =  "Themenliste:" + titles_str + prompt
-        prompt = [{"role": "user", "content":final_msg}]
+        prompt = systemMessage+[{"role": "user", "content":final_msg}]
         response = ollama.chat(model=modelID, messages=prompt)
         topics_dict[z['Name']] = response['message']['content'].split('\n')
     
