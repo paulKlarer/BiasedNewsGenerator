04482315360c340c00fb298a889e515dac5d1e22
Paul Klarer <paul.klarer@outlook.com>
2025-03-27 09:02:14 +0100

    CoT aus der Finetuned model antowrt entfernen

--- website/backend.py
+++ website/backend.py
@@ -221,8 +221,8 @@ def send_request_fine_tuned(prompt):
         resp.raise_for_status()
         data = resp.json()
         # Entferne den Prompt aus der Antwort
-        prompt_pattern = re.escape(f"{{'prompt': '{prompt}'}}")
-        data = re.sub(prompt_pattern, '', data).strip()
+        think_pattern = re.compile(r'.*?</think>', re.DOTALL)
+        data = re.sub(think_pattern, '', data).strip()
 
         return data.get("response", "No 'response' in JSON")
     except requests.exceptions.RequestException as e:
